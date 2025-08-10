2880a5e0b54ed8d2e55f78f9741d47c24dd4da74
Bheb Developer <bhebdeveloper@gmail.com>
2025-08-11 00:45:36 +0530

    MinSDK Changed from 34 to 32 for Moto G62

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.bmtcapitester"
-        minSdk = 34
+        minSdk = 32
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
